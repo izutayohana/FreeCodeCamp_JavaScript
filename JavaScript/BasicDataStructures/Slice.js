f9f099ed3d9d8f0073675e69a2d11c241c81f5e8
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-05 19:14:44 +0900

    First time using slice

---
+++ JavaScript/BasicDataStructures/Slice.js
@@ -0,0 +1,8 @@
+function happy(arr) {
+    // Only change code below this line
+  let greeting = arr.slice(2,6)
+    return greeting;
+  }
+  
+  // Only change code above this line
+  console.log(happy(['You', 'will', 'have', 'a', 'beautiful', 'day', 'tomorrow']));
