12410af1cef3a1bd89028c632f811e4e87ff63e9
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-05 19:27:53 +0900

    Shopping list using Spread Operator

--- JavaScript/BasicDataStructures/SpreadOperator.js
+++ JavaScript/BasicDataStructures/SpreadOperator.js
@@ -8,3 +8,15 @@ function copyMachine(arr, num) {
   }
   
   console.log(copyMachine(["cute", "capybara"], 5));
+
+  //Combine Arrays with the Spread Operator
+
+  function shoppinglist() {
+    let candy = ['chocolate', 'cake', 'gummy', 'soda'];
+    let sandwich = ['bread', 'cream cheese', 'ham', 'cheese']
+    let vegetables = ['lettuce', 'cabbage', 'onion']
+    let list = [...vegetables, ...candy, ...sandwich];
+    return list;
+  }
+  
+  console.log(shoppinglist());
