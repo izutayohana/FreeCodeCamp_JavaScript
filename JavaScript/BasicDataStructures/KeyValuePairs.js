8136df9175a66097b910d391f272d6beb8022577
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-06 18:21:21 +0900

    Learning what is a Key-Value pair and the syntaxes to add them in an existing object.

---
+++ JavaScript/BasicDataStructures/KeyValuePairs.js
@@ -0,0 +1,13 @@
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  
+  foods.bananas = 13; 
+  foods["grapes"] = 35; 
+  let redfruit = "strawberries";
+  foods[redfruit] = 27;
+
+  
+  console.log(foods);
