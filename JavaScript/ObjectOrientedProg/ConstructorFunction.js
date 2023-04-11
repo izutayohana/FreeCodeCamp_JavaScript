86a4d39bad63b1138267b79e17c498faaa9e2416
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-11 19:31:44 +0900

    Extend my constructor to receive arguments

--- JavaScript/ObjectOrientedProg/ConstructorFunction.js
+++ JavaScript/ObjectOrientedProg/ConstructorFunction.js
@@ -1,7 +1,9 @@
-function Dog() {
-    this.name = "Lilo";
-    this.color = "White with black dots";
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
     this.numLegs = 4;
     }
 
-    let hound = new Dog();
+    let hound = new Dog("Lilo", "White with black dots");
+    console.log(hound)
+    
