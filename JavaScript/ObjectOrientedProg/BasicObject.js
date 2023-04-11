953337c1810c8da113d2cb88ccce7c5d9ea82fab
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-11 19:04:25 +0900

    Learning the usage of .this inside an object.

--- JavaScript/ObjectOrientedProg/BasicObject.js
+++ JavaScript/ObjectOrientedProg/BasicObject.js
@@ -1,6 +1,6 @@
 let dog = {
     name: "Lilo",
     numLegs: 4,
-    sayLegs: function() {return "This dog has " + dog.numLegs + " legs."}
+    sayLegs: function() {return "This dog has " + this.numLegs + " legs."}
     };
     console.log(dog.sayLegs());
