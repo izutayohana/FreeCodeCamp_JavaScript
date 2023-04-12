06834851077b488f716816e77056cf6f2a838690
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-12 20:39:01 +0900

    Using prototype to avoid duplicating a lot of properties on a constructor function

---
+++ JavaScript/ObjectOrientedProg/Prototype.js
@@ -0,0 +1,12 @@
+//Properties in the prototype are shared among ALL instances of the constructor, it is better than have a lot of duplicated variables like we had before with numLegs. 
+
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 2
+  
+  // Only change code above this line
+  let beagle = new Dog("Snoopy");
+  
+  console.log(beagle)
