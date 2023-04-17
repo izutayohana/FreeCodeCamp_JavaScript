6b16f102846df9baf4d4ae46cb4b246392d260b7
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-17 20:06:48 +0900

    Final result

--- JavaScript/FunctionalProg/MapMethod.js
+++ JavaScript/FunctionalProg/MapMethod.js
@@ -114,8 +114,7 @@ const watchList = [
   
   // Only change code below this lineTitle
   
-
-const ratings = watchList.map(movie => {return(('{"title":  "' + movie["Title"] + '", "rating": "' + movie["imdbRating"] + '"}'))});
+const ratings = watchList.map(movie => ({title:  movie["Title"], rating: movie["imdbRating"]}));
 
 // Only change code above this line
 
