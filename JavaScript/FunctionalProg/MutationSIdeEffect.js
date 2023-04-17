7c4a23acadd61f4f027c21297d17bac99bc099fe
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-17 18:29:41 +0900

    Using the fixed value as an argument of my function

--- JavaScript/FunctionalProg/MutationSIdeEffect.js
+++ JavaScript/FunctionalProg/MutationSIdeEffect.js
@@ -3,7 +3,9 @@
 
 let fixedValue = 4;
 
-function incrementer() {
+//Another principle of functional programming is to always declare your dependencies explicitly. 
+//This means if a function depends on a variable or object being present, then pass that variable or object directly into the function as an argument.
+function incrementer(fixedValue) {
 let incremented = fixedValue+1;
 return incremented
 }
