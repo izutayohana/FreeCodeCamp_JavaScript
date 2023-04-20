73b3850a29549f5f50fa78316604bfac53f1afb1
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-20 17:17:50 +0900

    Learning about some method

---
+++ JavaScript/FunctionalProg/SomeMethd.js
@@ -0,0 +1,10 @@
+//Use the some method inside the checkPositive function to check if any element in arr is positive. The function should return a Boolean value.
+
+function checkPositive(arr) {
+    // Only change code below this line
+  return (arr.some(function (value) {return value > 0;}))
+  
+    // Only change code above this line
+  }
+  
+  console.log(checkPositive([1, 2, 3, -4, 5]));
