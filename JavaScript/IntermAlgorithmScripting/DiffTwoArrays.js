a4d49a1fb70d7f43f6251d4f8f158a985568f7ad
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-20 18:45:45 +0900

    Correcting in order to fit for arrays with both strings and numbers

--- JavaScript/IntermAlgorithmScripting/DiffTwoArrays.js
+++ JavaScript/IntermAlgorithmScripting/DiffTwoArrays.js
@@ -1,12 +1,26 @@
 //Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 
 function diffArray(arr1, arr2) {
-    const newArr = arr1.slice().concat(arr2).sort(function(a, b) {return a===b ? 0: a > b? 1 : -1});
+    const newArr = arr1.slice().concat(arr2)
+    const numbers = [];
+    const strings = [];
+    for (let i = 0; i < newArr.length; i ++){
+      if (typeof(newArr[i]) == "number") {
+        numbers.push(newArr[i]);
+      } else {
+        strings.push(newArr[i]);
+      }
+    }
+    strings.sort(function(a, b) {return a===b ? 0: a > b? 1 : -1});
+    numbers.sort(function(a, b){return a-b});
+    let allsorted = strings.concat(numbers);
+    console.log(allsorted)
     let different = [];
-    for (let i = 0; i < newArr.length; i+=2){
-      if (newArr[i] != newArr[i+1]){
-        different.push(newArr[i]);
-        i++;
+    for (let j = 0; j < allsorted.length; j++){
+      if (allsorted[j] == allsorted[j+1]){
+        j++;
+      } else {
+        different.push(allsorted[j])
       }
     }
     return different;
