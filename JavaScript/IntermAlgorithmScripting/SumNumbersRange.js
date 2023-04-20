f25117f31e55458302f8bc247db8c7d5a46c4f5f
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-20 17:37:21 +0900

    Sorting numerically the numbers

---
+++ JavaScript/IntermAlgorithmScripting/SumNumbersRange.js
@@ -0,0 +1,9 @@
+function sumAll(arr) {
+    let organize = arr.slice(0).sort(function (a, b){return a-b})
+    //if (arr[0] > arr [1]) {
+   //   for 
+   // }
+    return organize;
+  }
+  
+  console.log(sumAll([4, 1]));
