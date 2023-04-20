26a6ff7cda333bf1bea5771f96abef1a9c7160ab
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-20 17:54:52 +0900

    Return the sum of two numbers in a given array  plus the sum of all the numbers between them.

--- JavaScript/IntermAlgorithmScripting/SumNumbersRange.js
+++ JavaScript/IntermAlgorithmScripting/SumNumbersRange.js
@@ -1,11 +1,15 @@
+//We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+
+//For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+
 function sumAll(arr) {
     arr.sort(function (a, b){return a-b})
     let full = [];
     for (let i = arr[0]; i <= arr[1]; i++) {
     full.push(i);
     }
-    return full;
+    let sums = full.reduce((sum, value) => (sum + value))
+    return sums;
   }
   
-  console.log(sumAll([4, 1]));
-  
+  console.log(sumAll([5, 10]));
