10f8223be7fe6fde74e40dd1fd9185062b1f23e3
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-20 17:52:56 +0900

    Creating a new array with all the numbers in between the arguments

--- JavaScript/IntermAlgorithmScripting/SumNumbersRange.js
+++ JavaScript/IntermAlgorithmScripting/SumNumbersRange.js
@@ -1,9 +1,11 @@
 function sumAll(arr) {
-    let organize = arr.slice(0).sort(function (a, b){return a-b})
-    //if (arr[0] > arr [1]) {
-   //   for 
-   // }
-    return organize;
+    arr.sort(function (a, b){return a-b})
+    let full = [];
+    for (let i = arr[0]; i <= arr[1]; i++) {
+    full.push(i);
+    }
+    return full;
   }
   
   console.log(sumAll([4, 1]));
+  
