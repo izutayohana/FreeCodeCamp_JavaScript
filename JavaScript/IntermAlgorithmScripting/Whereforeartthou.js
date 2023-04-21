e1576bb1ae560d9a3aace1d7b7cd5fb73c679a8e
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-21 19:35:53 +0900

    Part 2 with error

--- JavaScript/IntermAlgorithmScripting/Whereforeartthou.js
+++ JavaScript/IntermAlgorithmScripting/Whereforeartthou.js
@@ -1,13 +1,17 @@
+//Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). 
+//Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
 function whatIsInAName(collection, source) {
     let keys = Object.keys(source);
     let newArr = [];
     for (let i = 0; i < keys.length; i++){
     let items = source[keys[i]]
-    return items
+    if (collection.some(e => e[keys[i]] === items)) {
+     let index = collection.findIndex(e => e[keys[i]] === items)
+     newArr.push(collection[index]);
+  return newArr
     }
-  
-  
-    
+    }
   }
   
   console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
