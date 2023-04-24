b859fc526e03750b7705666638a122e26ee8fd32
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-24 19:21:44 +0900

    Final answer

--- JavaScript/IntermAlgorithmScripting/Whereforeartthou.js
+++ JavaScript/IntermAlgorithmScripting/Whereforeartthou.js
@@ -14,4 +14,4 @@ function whatIsInAName(collection, source) {
   });
 }
 
-  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+  console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }));
