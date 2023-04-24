c4876e291929addf45fb522003426215029119a8
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-24 17:28:10 +0900

    Final answer

--- JavaScript/IntermAlgorithmScripting/Whereforeartthou.js
+++ JavaScript/IntermAlgorithmScripting/Whereforeartthou.js
@@ -2,16 +2,16 @@
 //Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
 
 function whatIsInAName(collection, source) {
-    let keys = Object.keys(source);
-    let newArr = [];
-    for (let i = 0; i < keys.length; i++){
-    let items = source[keys[i]]
-    if (collection.some(e => e[keys[i]] === items)) {
-     let index = collection.findIndex(e => e[keys[i]] === items)
-     newArr.push(collection[index]);
-  return newArr
+  let keys = Object.keys(source);
+  return collection.filter(obj => {
+    for (let i = 0; i < keys.length; i++) {
+      if (!obj.hasOwnProperty(keys[i]) ||
+          obj[keys[i]] !== source[keys[i]]) {
+        return false;
+      }
     }
-    }
-  }
-  
+    return true;
+  });
+}
+
   console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
