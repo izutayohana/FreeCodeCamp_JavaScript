c2f229b07381fb3fa26be052149b78d23d7ed4cb
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-26 20:01:19 +0900

    Creating a function to convertcharacters in a string to their corresponding HTML entities.

---
+++ JavaScript/IntermAlgorithmScripting/HTMLEntities.js
@@ -0,0 +1,7 @@
+//Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+// & = &amp;
+// < = &lt;
+// > = &gt;
+// "" = &quot;
+// '' = &apos;
+
