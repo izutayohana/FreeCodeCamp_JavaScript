4a0259a7a0b8ace442e86a7e204070daa167cbb4
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-04-27 17:59:40 +0900

    Returning a function that returns the sum of all prime numbers that are less than or equal to num.

--- JavaScript/IntermAlgorithmScripting/SumPime.js
+++ JavaScript/IntermAlgorithmScripting/SumPime.js
@@ -1,19 +1,19 @@
 //Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
 
 function sumPrimes(num) {
-    let sum = 1;
+    let sum = 0;
     let counter = 0;
-    for (let j = 1; j < num; j++){
-      for (let i = 1; i < 10; i++) {
-        if (j % i == 0){
+    for (let j = 2; j <= num; j++){
+      for (let i = 1; i <= j; i++){
+        if(j % i == 0){
           counter++;
-        } if(counter == 2) {
-          sum = sum + j
-          console.log(sum)
-        }  
-    }
+        }
+      } if (counter <= 2) {
+        sum = sum + j;
+        console.log(j)
+      } counter = 0;
     }
     return sum;
   }
   
-  console.log(sumPrimes(10));   
+  console.log(sumPrimes(10));
