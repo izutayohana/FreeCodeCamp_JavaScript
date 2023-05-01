b8a4bdd6c1a2c546e6dd97c3d1b64b8cfc13c8c7
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-05-01 18:04:45 +0900

    Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.

--- JavaScript/FunctionalProg/DropIt.js
+++ JavaScript/FunctionalProg/DropIt.js
@@ -1,13 +1,20 @@
 function dropElements(arr, func) {
-    let newArr = [];
-    
-       for (let j = 0; j < arr.length; j++){
-         while (!func(arr[j])) {
-           arr.shift();
-       }
-  
-    } 
-    return arr;
-  }
-  
-  console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}));
+      
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+}
+
+return arr;
+}
+
+console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}));
+
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}));
+
+console.log(dropElements([1, 2, 3], function(n) {return n > 0;}));
+
+console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));
+
+console.log(dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}));
+
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}));
