a2939d009e4fd74873aeb21adaea3ff0bcb8b991
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-05-09 14:43:21 +0900

    Return true if the passed string looks like a valid US phone number.

---
+++ JavaScript/FinalProjects/TelephoneValid.js
@@ -0,0 +1,3 @@
+//Return true if the passed string looks like a valid US phone number.
+
+//The user may fill out the form field any way they choose as long as it has the format of a valid US number. 
